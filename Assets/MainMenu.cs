fd01d7c37ac239d774444610aa8cb172f8236565
Nahn2051 <dtnha05@gmail.com>
2025-05-13 13:41:17 +0700

    Update button and Menu UI
    Update button and Menu UI

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,19 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Button StartButton;
+    public void LoadJoinScene() {
+        SceneManager.LoadScene("JoinScene");
     }
     public void LoadProfileScene()
     {
